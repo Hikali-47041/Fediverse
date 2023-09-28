3b37210a3a1ef6370c1d871fa35e987c7ff9f231
Hikaru <34558365+Hikali-47041@users.noreply.github.com>
2023-09-28 21:35:03 +0900

    Add: debug message

--- scripts/akkoma-backup.sh
+++ scripts/akkoma-backup.sh
@@ -10,20 +10,27 @@ dbname="akkoma"
 dbuser="postgres"
 filename="hi-47041-net_$(date -I).pgdump"
 backup_file_amount=5
+debug_flag=true
+
+# message
+echo "akkoma-backup.sh | akkoma-backup.sh [info] backup job started"
 
 # mkdir if $savedir does not exist
 if [ ! -e "$savedir" ]
 then
     mkdir -p "$savedir"
+    if "$debug_flag"; then echo "akkoma-backup.sh | [debug] mkdir $savedir"; fi
 fi
 
 # backup
 cd "$workdir" || exit 1
 su "$dbuser" -lc "pg_dump -d $dbname --format=custom -f "$workdir/"$filename"""
+if "$debug_flag"; then echo "akkoma-backup.sh [debug] pg_dump $workdir/$filename created!"; fi
 mv "$workdir/$filename" "$savedir"
 
 # remove older backup data
 while [ "$(find "$savedir" -type f | wc -l)" -ge "$backup_file_amount" ]
 do
+    if "$debug_flag"; then echo "akkoma-backup.sh [debug] old pg_dump $savedir/$(find "$savedir" -type f | sort | head -1) removed!"; fi
     rm "$savedir/$(find "$savedir" -type f | sort | head -1)"
 done
